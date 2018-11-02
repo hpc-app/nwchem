fbbf02e7b1f88076dcc6b11c9f035eced043a81d
A_Xuan <XuanBaby_1993@163.com>
2018-11-02 17:17:45 +0800

    Add files via upload

--- src/util/util_cuda_support.c
+++ src/util/util_cuda_support.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef TCE_CUDA
 #ifdef OLD_CUDA
 #include <cuda_runtime_api.h>
 #else
 #include <cuda.h>
 #endif
+#endif
+#ifdef TCE_HIP
+#include <hip/hip_runtime_api.h>
+#endif
 #include "ga.h"
 #include "typesf2c.h"
+#ifdef TCE_CUDA
 Integer FATR util_cuda_get_num_devices_(){
   int dev_count_check;
   cudaGetDeviceCount(&dev_count_check);
   return (Integer) dev_count_check;
 }
+#endif
+#ifdef TCE_HIP
+Integer FATR util_cuda_get_num_devices_(){
+  int dev_count_check;
+  hipGetDeviceCount(&dev_count_check);
+  return (Integer) dev_count_check;
+}
+#endif
 /* $Id$ */
